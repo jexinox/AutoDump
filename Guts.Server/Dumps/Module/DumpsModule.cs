87600b0e6cf55cd2f48afa893c61490360a1a581
jexinox <jexinox@gmail.com>
2024-03-17 17:38:00 +0500

    todo and produces

--- Guts.Server/Dumps/Module/DumpsModule.cs
+++ Guts.Server/Dumps/Module/DumpsModule.cs
@@ -19,6 +19,7 @@ public class DumpsModule : IApiModule
 
     public IEndpointRouteBuilder MapEndpoints(IEndpointRouteBuilder endpoints)
     {
+        // TODO (jexinox): faults mapping
         endpoints
             .MapPost(
                 "/api/v1/dumps/{hostName}",
@@ -30,7 +31,9 @@ public class DumpsModule : IApiModule
                     var command = new UploadDumpCommand(new(hostName), new(dumpArchive));
                     return await handler.Handle(command).MapFault(_ => Results.Ok());
                 })
-            .Accepts<Stream>("application/octet-stream");
+            .Accepts<Stream>("application/octet-stream")
+            .Produces(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status500InternalServerError);
 
         return endpoints;
     }
--- Guts.Server/Dumps/Module/UploadDumpRequest.cs
+++
@@ -1,5 +0,0 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-namespace Guts.Server.Dumps.Module;
-
-public record UploadDumpRequest([FromRoute] string HostName, [FromBody] Stream DumpArchive);
