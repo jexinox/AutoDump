e16fcee4950b4f764323292b7ac02132e654419d
jexinox <jexinox@gmail.com>
2025-03-17 21:39:41 +0500

    small refactoring

--- Guts.Analyzer/DataProviders/ObjectsDistributionByGenerationsDataProvider.cs
+++ Guts.Analyzer/DataProviders/ObjectsDistributionByGenerationsDataProvider.cs
@@ -5,23 +5,27 @@ namespace Guts.Analyzer.DataProviders;
 public class ObjectsDistributionByGenerationsDataProvider
 {
     private readonly IClrRuntimeProvider _runtimeProvider;
+    private readonly ObjectsTreeProvider _objectsTreeProvider;
 
     public ObjectsDistributionByGenerationsDataProvider(
-        IClrRuntimeProvider runtimeProvider)
+        IClrRuntimeProvider runtimeProvider,
+        ObjectsTreeProvider objectsTreeProvider)
     {
         _runtimeProvider = runtimeProvider;
+        _objectsTreeProvider = objectsTreeProvider;
     }
 
     public IReadOnlyDictionary<Generation, IReadOnlyList<(MemoryRange GenerationRange, IReadOnlyList<ClrObject> ObjectsRanges)>> Get()
     {
         var heap = _runtimeProvider.GetCurrentDumpRuntime().Heap;
         var generationsRanges = new Dictionary<Generation, List<(MemoryRange, IReadOnlyList<ClrObject>)>>();
+        var objectsTree = _objectsTreeProvider.Get();
 
         foreach (var segmentGenerationRanges in heap.Segments.Select(GetGenerationsRangesBySegment))
         {
             foreach (var (generation, memoryRange) in segmentGenerationRanges)
             {
-                var objects = GetObjectsInMemoryRange(memoryRange, heap);
+                var objects = GetObjectsInMemoryRange(memoryRange, heap, objectsTree.ObjectNodes.ContainsKey);
                 if (generationsRanges.TryGetValue(generation, out var generationRanges))
                 {
                     generationRanges.Add(new(memoryRange, objects));
@@ -59,11 +63,11 @@ public class ObjectsDistributionByGenerationsDataProvider
         };
     }
 
-    private static List<ClrObject> GetObjectsInMemoryRange(MemoryRange memoryRange, ClrHeap heap)
+    private static List<ClrObject> GetObjectsInMemoryRange(MemoryRange memoryRange, ClrHeap heap, Func<ulong, bool> isReachable)
     {
         return heap
             .EnumerateObjects(memoryRange)
-            .Where(obj => obj is { IsFree: false })
+            .Where(obj => isReachable(obj))
             .ToList();
     }
 }
--- Guts.Analyzer/Program.cs
+++ Guts.Analyzer/Program.cs
@@ -1,25 +1,23 @@
 ﻿using Guts.Analyzer;
 using Guts.Analyzer.DataProviders;
+using Guts.Analyzer.Statistics;
 using Microsoft.Diagnostics.Runtime;
 
 var dump = DataTarget.LoadDump("./Dumps/test-dump-net6s.dmp");
 
 var clr = dump.ClrVersions[0].CreateRuntime();
 
-// foreach (var heap in clr.EnumerateClrNativeHeaps())
-// {
-//     Console.WriteLine(heap.Kind);
-//     Console.WriteLine(heap.GCHeap);
-// }
 var clrProvider = new StubClrRuntimeProvider(clr);
-// var objectsDistributionDataProvider = new ObjectsDistributionByGenerationsDataProvider(clrProvider);
 var objectsTreeProvider = new ObjectsTreeProvider(clrProvider);
-var fragmentationDataProvider = new GenerationsSizesDataProvider(clrProvider, objectsTreeProvider);
+var objectsDistributionDataProvider = new ObjectsDistributionByGenerationsDataProvider(clrProvider, objectsTreeProvider);
+var generationsSizesDataProvider = new GenerationsSizesStatProvider(objectsDistributionDataProvider);
 
 Console.WriteLine($"Is server: {clr.Heap.IsServer}");
 Console.WriteLine($"Uses regions: {clr.Heap.Segments.Any(segment => segment.Kind is GCSegmentKind.Generation0 or GCSegmentKind.Generation1)}");
 
-foreach (var (generation, fragmentation) in fragmentationDataProvider.Get())
+foreach (var (generation, size) in generationsSizesDataProvider.Get())
 {
-    Console.WriteLine($"{generation}, Total (B): {fragmentation.Total}, Total (MB): {fragmentation.Total / 1024d / 1024d}, Used (B): {fragmentation.Used} Used (KB): {fragmentation.Used / 1024d}, Used (MB): {fragmentation.Used / 1024d / 1024d}");
+    Console.WriteLine(
+        $"{generation}, Total (B): {size.Total}, Total (MB): {size.Total / 1024d / 1024d}, " +
+        $"Used (B): {size.Used} Used (KB): {size.Used / 1024d}, Used (MB): {size.Used / 1024d / 1024d}");
 }
