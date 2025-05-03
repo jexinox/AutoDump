6fa7ab525c5fde69ba7076a4de2c056f1be326b1
a.karpov <a.karpov@skbkontur.ru>
2025-05-03 13:07:18 +0500

    refactored Program.cs

--- Guts.DirectoryWatcher/Program.cs
+++ Guts.DirectoryWatcher/Program.cs
@@ -1,14 +1,15 @@
 ﻿using Guts.DirectoryWatcher;
 using Refit;
 
+var serverAddress = Environment.GetEnvironmentVariable("GUTS_SERVER_ADDRESS") ?? 
+                    throw new("GUTS_SERVER_ADDRESS environment variable was not found");
+var client = RestService.For<IGutsServerClient>(serverAddress);
+
 var dumpFilesMask = Environment.GetEnvironmentVariable("GUTS_DUMPS_MASK") ?? "*";
-var directory = Environment.GetEnvironmentVariable("GUTS_DUMPS_DIRECTORY");
-if (directory is null)
-{
-    throw new("GUTS_DUMPS_DIRECTORY environment variable was not found");
-}
-
+var directory = Environment.GetEnvironmentVariable("GUTS_DUMPS_DIRECTORY") ??
+                throw new("GUTS_DUMPS_DIRECTORY environment variable was not found");
 var dumpsWatcher = new DumpsFileSystemWatcher(directory, dumpFilesMask);
+var changes = dumpsWatcher.CreateChangesChannel();
 
-
-
+var dumpsEventsWatcher = new DumpsEventsWatcher(changes, client);
+await dumpsEventsWatcher.Start();
